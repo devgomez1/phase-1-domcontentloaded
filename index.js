4c984ee9f30d4bbece069cbe6a434c0d019a3509
Devin Elliot Gomez <devinelliotgomez@gmail.com>
2023-11-07 09:27:06 -0600

    finished lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+const exampleText = document.getElementById("text");
+
+document.addEventListener("DOMContentLoaded", function () {
+  console.log("The DOM has loaded");
+  exampleText.innerHTML = "This is really cool!";
+});
